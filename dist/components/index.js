a6c01b8af1497afeef9aa5642a0f08adf233b96e
mangulom <mauricioangulo4@gmail.com>
2025-12-01 12:55:31 -0500

    Commit colores de botones corregidos

--- dist/components/index.js
+++
@@ -1,4 +0,0 @@
-export { getAssetPath, render, setAssetPath, setNonce, setPlatformOptions } from '@stencil/core/internal/client';
-//# sourceMappingURL=index.js.map
-
-//# sourceMappingURL=index.js.map
